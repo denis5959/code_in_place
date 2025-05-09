8a642b10feacd77ed07af3cf844d80bc2b9743cf
Denis <A01410094@tec.mx>
2025-05-09 12:01:47 -0600

    Update MarsWeight.py

--- Week3/MarsWeight.py
+++ Week3/MarsWeight.py
@@ -1,9 +1,9 @@
-"""
-Prompts the user for a weight on Earth
-and prints the equivalent weight on Mars.
-"""
+MARS_CONSTANT = 0.378
 
 def main():
+    weight_earth = input("Enter a weight on Earth: ")
+    weight_earth = MARS_CONSTANT*int(weight_earth)
+    print("The equivalent weight on Mars: " + str(weight_earth))
     # Fill this function out!
     pass
 
