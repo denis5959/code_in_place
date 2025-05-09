b869c597125aa70d34c34aec76e6f348c1f7919f
Denis <A01410094@tec.mx>
2025-05-09 11:56:43 -0600

    Create MarsWeight.py

---
+++ Week3/MarsWeight.py
@@ -0,0 +1,11 @@
+"""
+Prompts the user for a weight on Earth
+and prints the equivalent weight on Mars.
+"""
+
+def main():
+    # Fill this function out!
+    pass
+
+if __name__ == "__main__":
+    main()
