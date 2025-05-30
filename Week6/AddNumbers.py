dbf2e7ccf4244d88770c8852b340d3ea59d3c73a
Denis <A01410094@tec.mx>
2025-05-30 12:09:42 -0600

    Create AddNumbers.py

---
+++ Week6/AddNumbers.py
@@ -0,0 +1,9 @@
+def add_many_numbers(numbers):
+  total_so_far = 0 
+  for number in numbers:
+    total_so_far+= number
+  return total_so_far
+
+numbers = [1,2,3,4,5]
+sum_of_numbers = add_many_numbers(numbers)
+print(sum_od¿f_numbers)
